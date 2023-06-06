c0f320253600fa0792777260a32c0df8ff89d06a
anderssonw <wandersson.98@gmail.com>
2023-06-06 10:57:31 +0200

    stylemaster stuff
    Signed-off-by: anderssonw <wandersson.98@gmail.com>

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,16 +1,29 @@
 import { Box, Button, Flex, Text } from "@kvib/react";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function NavBar() {
+  const styleNoFlex =
+    "hidden bg-green-50 -mx-6 mt-6 absolute shadow flex-col text-center z-10 animate-scale-from-nothing ";
+  const styleFlex = styleNoFlex + " group-hover:flex";
+  const [dropDownStyle, setdropDownStyle] = useState(styleFlex);
+
+  const switchDropDownStyle = () => {
+    setdropDownStyle(styleNoFlex);
+    setTimeout(() => {
+      setdropDownStyle(styleFlex);
+    }, 100);
+  };
+
   return (
-    <nav className="flex flex-row justify-center">
+    <nav className="flex flex-row justify-around">
       <Box className="p-6 cursor-pointer hover:bg-green-100">
         <Link href={""}>Om Kompass</Link>
       </Box>
 
       <Box className="p-6 group cursor-pointer overflow-hidden hover:bg-green-100">
         Teamene
-        <Box className="hidden bg-green-50 absolute shadow flex-col text-center z-10 max-w-sm animate-scale-from-nothing group-hover:flex">
+        <Box className={dropDownStyle} onClick={switchDropDownStyle}>
           <Link className="p-3 hover:bg-green-100" href={"/dask"}>
             DASK
           </Link>
